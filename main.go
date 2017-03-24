553e5a33d4d10f3bac8722a77647eb8b762ec5c0
James Dunne <james.jdunne@gmail.com>
2017-03-23 21:55:07 -0500

    Made more generic with commandline flags.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,17 +11,19 @@ import (
 	"regexp"
 )
 
-type FileInfoPath struct {
-	FileInfo os.FileInfo
-	Dir string
-} 
-
 func main() {
-	localPath := `F:\Video\TV\The X-Files\`
+	path := flag.String("path", ".", "local path")
+	isRecursive := flag.Bool("recurse", false, "recurse subdirectories")
+	matchRe := flag.String("match", "(.*)", "regexp match")
+	replaceRe := flag.String("replace", "", "regexp replace")
+	doReplace := flag.Bool("exec", false, "actually rename files")
+	flag.Parse()
 
-	files := make([]FileInfoPath, 0, 50)
+	// `^(.*) Season (\d\d) Episode (\d\d) - (.*)$`
+	re := regexp.MustCompile(*matchRe)
+
 	dirs := make([]string, 0, 50)
-	dirs = append(dirs, localPath)
+	dirs = append(dirs, *path)
 
 	for (len(dirs) > 0) {
 		// Open the directory to read its contents:
@@ -43,33 +46,37 @@ func main() {
 
 		for _, fi := range fis {
 			if fi.IsDir() {
-				// Skip folders:
-				if fi.Name() == "Extras" {
+				if !*isRecursive {
 					continue
 				}
+
 				dirs = append(dirs, filepath.Join(dirPath, fi.Name()))
 				continue
 			}
 
-			if filepath.Ext(fi.Name()) != ".avi" {
+			oldName := fi.Name()
+			newName := oldName
+			if *replaceRe == "" {
+				if re.MatchString(oldName) {
+					fmt.Printf("%s\n", filepath.Join(dirPath, oldName))
+				}
 				continue
 			}
 
-			files = append(files, FileInfoPath{FileInfo: fi, Dir: dirPath})
+			// "${1} S${2}E${3} - ${4}"
+			newName = re.ReplaceAllString(oldName, *replaceRe)
+			if (oldName == newName) {
+				continue
+			}
+
+			fmt.Printf("%s%c{%s -> %s}\n", dirPath, filepath.Separator, oldName, newName)
+			if *doReplace {
+				// Rename file:
+				os.Rename(
+					filepath.Join(dirPath, oldName),
+					filepath.Join(dirPath, newName),
+				)
+			}
 		}
-	}
-
-	re := regexp.MustCompile(`^(.*) Season (\d\d) Episode (\d\d) - (.*)$`)
-	for _, fpath := range files {
-		oldName := fpath.FileInfo.Name()
-		newName := re.ReplaceAllString(oldName, "${1} S${2}E${3} - ${4}")
-
-		if (oldName == newName) {
-			continue
-		}
-
-		// Rename file:
-		fmt.Printf("%s\n", filepath.Join(fpath.Dir, newName))
-		os.Rename(filepath.Join(fpath.Dir, oldName), filepath.Join(fpath.Dir, newName))
 	}
 }
